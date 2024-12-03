c8c004c32d5d1fb7c6004b0e3036006f4fcd8387
brodkin-3 <121911359+brodkin-3@users.noreply.github.com>
2024-12-03 10:16:20 -0500

    challenge

--- components/ProductDisplay.js
+++ components/ProductDisplay.js
@@ -39,6 +39,13 @@ app.component('product-display', {
           v-on:click="addToCart">
           Add to Cart
         </button>
+
+        <button 
+          class="button" 
+        :disabled="!inStock" 
+          v-on:click="removeFromCart">
+          Remove
+        </button>
       </div>
     </div>
   </div>`,
@@ -56,7 +63,10 @@ app.component('product-display', {
   },
   methods: {
       addToCart() {
-        this.$emit('add-to-cart')
+        this.$emit('add-to-cart', this.variants[this.selectedVariant].id)
+      },
+      removeFromCart() {
+        this.$emit('remove-from-cart', this.variants[this.selectedVariant].id)
       },
       updateVariant(index) {
           this.selectedVariant = index
